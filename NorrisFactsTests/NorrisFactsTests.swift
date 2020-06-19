736c76e006aefe165b0f1f162022a1d9a0071b65
Alan Paiva <jpaiva.alan@gmail.com>
2020-06-19 07:32:57 -0300

    Add Quick & Nimble

--- NorrisFactsTests/NorrisFactsTests.swift
+++ NorrisFactsTests/NorrisFactsTests.swift
@@ -6,29 +6,15 @@
 //  Copyright © 2020 Alan Paiva. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
 @testable import NorrisFacts
 
-class NorrisFactsTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class NorrisFactsSpec: QuickSpec {
+    override func spec() {
+        it("examples") {
+            let count = 1
+            expect(count).to(equal(1))
         }
     }
-
 }
--- Podfile
+++ Podfile
@@ -13,5 +13,8 @@ target 'NorrisFacts' do
 
   target 'NorrisFactsTests' do
     inherit! :search_paths
+
+    pod 'Quick', '~> 2.0'
+    pod 'Nimble', '~> 8.0'
   end
 end
